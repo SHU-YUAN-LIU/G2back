1332abaa33c75e98b40bc2bf6241a128fac665b3
wendychen0731 <sun609099@gmail.com>
2024-02-04 14:23:00 +0800

    管理員帳戶頁面

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,7 +1,29 @@
 
 
 <template>
-  
+<div class="admin">
+    <MainHeader />
+    <table>
+        <tr>
+            <td>管理員編號</td>
+            <td>姓名</td>
+            <td>權限等級</td>
+            <td>啟用狀態</td>
+            <td>操作</td>
+        </tr>
+    </table>
+</div>
 </template>
-<script setup>
+<script>
+import MainHeader from "../components/MainHeader.vue";
+export default{
+    data(){
+        return{
+
+        };
+    },
+    components:{
+        MainHeader,
+    },
+}
 </script>
