5ed383058e19e21bcd809bce5dc66c58a9c502b0
105021113 <zoozootom@gmail.com>
2024-02-06 17:38:51 +0800

    add modify_date change

--- src/views/MemberView.vue
+++ src/views/MemberView.vue
@@ -23,7 +23,7 @@
                 <td class="align-middle">{{ item.member_name }}</td>
                 <td class="align-middle">{{ item.cellphone }}</td>
                 <td class="align-middle">
-                  <switch_btn />
+                  <switch_btn :ischecked="item.status == '正常'" @change="changeMemberStatus(item.member_no)" />
                 </td>
 
                 <td class="align-middle">
@@ -38,55 +38,56 @@
       </div>
     </div>
   </div>
-  <Lightbox class="memberlightbox" @toSaveData="updateData(currentightbox[0].member_no)" lightboxType="true" ref="lightbox">
+  <Lightbox class="memberlightbox" @toSaveData="updateData(currentlightbox[0].member_no)" lightboxType="true"
+    ref="lightbox">
 
     <div class="admin_lightbox" style="overflow-y: scroll;">
       <p>
         <span>建立日期: </span>
-        <span>2024/1/1</span>
+        <span>{{ currentlightbox[0].create_date }}</span>
       </p>
       <p>
         <span>修改日期: </span>
-        <span>2024/1/1</span>
+        <span>{{ currentlightbox[0].modify_date }}</span>
       </p>
       <p class="title"><span>詳細資訊</span></p>
       <table>
         <tr>
           <td>會員ID:</td>
-          <td>{{ currentightbox[0].member_no }}</td>
+          <td>{{ currentlightbox[0].member_no }}</td>
         </tr>
         <tr>
           <td>姓名:</td>
-          <td>{{ currentightbox[0].member_name }}</td>
+          <td>{{ currentlightbox[0].member_name }}</td>
         </tr>
         <tr>
           <td>生日:</td>
-          <td>{{ currentightbox[0].birthday }}</td>
+          <td>{{ currentlightbox[0].birthday }}</td>
         </tr>
         <tr>
           <td>地址:</td>
-          <td>{{ currentightbox[0].address }}</td>
+          <td>{{ currentlightbox[0].address }}</td>
         </tr>
         <tr>
           <td>行動電話:</td>
-          <td>{{ currentightbox[0].cellphone }}</td>
+          <td>{{ currentlightbox[0].cellphone }}</td>
         </tr>
         <tr>
           <td>室內電話:</td>
-          <td>{{ currentightbox[0].phone }}</td>
+          <td>{{ currentlightbox[0].phone }}</td>
         </tr>
         <tr>
           <td>EMAIL:</td>
-          <td>{{ currentightbox[0].email }}</td>
+          <td>{{ currentlightbox[0].email }}</td>
         </tr>
         <tr>
           <td>密碼:</td>
-          <td>{{ currentightbox[0].password }}</td>
+          <td>{{ currentlightbox[0].password }}</td>
         </tr>
         <tr>
           <td>狀態:</td>
           <td>
-            <select id="member_status" :value="currentightbox[0].status">
+            <select id="member_status" :value="currentlightbox[0].status">
               <option value="正常">正常</option>
               <option value="停用">停用</option>
             </select>
@@ -94,7 +95,7 @@
         </tr>
         <tr>
           <td>點數:</td>
-          <td>{{ currentightbox[0].point }}</td>
+          <td>{{ currentlightbox[0].point }}</td>
         </tr>
 
       </table>
@@ -120,8 +121,8 @@ export default {
     return {
       placeholder: '會員ID或電話',
       memberdata: [],
-      currentightbox:[],
-      memberstatus:'',
+      currentlightbox: [],
+      memberstatus: '',
 
     }
   },
@@ -134,21 +135,22 @@ export default {
   methods: {
     showLightbox(id) {
       this.$refs.lightbox.showLightbox = true;
-      console.log(id);
-      this.currentightbox=this.memberdata.filter((item)=>{
-        return item.member_no==id
+
+      // console.log(id);
+      this.currentlightbox = this.memberdata.filter((item) => {
+        return item.member_no == id
       })
-      console.log(this.currentightbox);
+      // console.log(this.currentlightbox);
     },
     getMemberData() {
-      this.memberdata= []
-      this.currentightbox=[]
+      this.memberdata = [];
+      this.currentlightbox = [];
       let url = `${import.meta.env.VITE_API_URL}/memberDataGetAll.php`;
       fetch(url)
         .then(response => response.json())
         .then(result => {
           const members = result.members;
-          console.log(111, result)
+          // console.log(111, result)
 
           this.showMembers(members);
         })
@@ -159,11 +161,11 @@ export default {
     showMembers(members) {
 
       for (let i = 0; i < members.length; i++) {
-        if(members[i].status=='A'){
-            this.memberstatus='正常'
-          }else{
-            this.memberstatus='停用'
-          }
+        if (members[i].status == 'A') {
+          this.memberstatus = '正常'
+        } else {
+          this.memberstatus = '停用'
+        }
         this.memberdata.push({
           member_no: members[i].member_no,
           member_name: members[i].member_name,
@@ -177,18 +179,47 @@ export default {
           point: members[i].point,
           modifier: members[i].modifier,
           modify_date: members[i].modify_date,
-          status:this.memberstatus,
+          create_date: members[i].create_date,
+          status: this.memberstatus,
         })
 
       }
-      console.log(this.memberdata);
+      // console.log(this.memberdata);
+    },
+    changeMemberStatus(member_no) {
+      const member = this.memberdata.find(item => item.member_no === member_no);
+      if (member) {
+        member.status = (member.status === '正常') ? '停用' : '正常';
+        // console.log(member.status);
+        if (member.status == '正常') {
+          this.memberstatus = 'A'
+        } else {
+          this.memberstatus = 'IA'
+        }
+        member.modify_date = new Date().toLocaleString();
+        let memberData = {
+          member_no,
+          status: this.memberstatus,
+        };
+        let url = `${import.meta.env.VITE_API_URL}/memberDataUpdate.php`;
+        fetch(url, {
+          method: 'post',
+          headers: {
+            "content-type": "application/json"
+          },
+          body: JSON.stringify(memberData)
+        })
+          .then(response => response.json())
+          .catch(error => console.log(error))
+      }
     },
     updateData(member_no) {
-      if(document.getElementById("member_status").value=='正常'){
-        this.memberstatus='A'
-      }else{
-        this.memberstatus='IA'
+      if (document.getElementById("member_status").value == '正常') {
+        this.memberstatus = 'A'
+      } else {
+        this.memberstatus = 'IA'
       }
+      // console.log(this.memberdata.status);
       let memberData = {
         member_no,
         status: this.memberstatus,
