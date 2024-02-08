c7005f699462cd93b5b6602e17f6013c4b570923
Min-Syuan <annie0910610569gmail.com>
2024-02-09 00:02:03 +0800

    燈箱

--- src/views/BannerView.vue
+++ src/views/BannerView.vue
@@ -1,34 +1,39 @@
 <template>
     <MainHeader />
-    <Dropdown />
+    <dropDown />
     <div class="banner">
         <div class="banner_container">
-            <table>
-                <thead>
-                    <td>消息編號</td>
-                    <td>輪播圖預覽</td>
-                    <td>主旨</td>
-                    <td>啟用狀態</td>
-                    <td>操作</td>
-                </thead>
-                <tbody>
-                    <tr :key="index" v-for="index in 6">
-                        <td class="banner_id">1</td>
-                        <td class="banner_pic">
-                            <img src="../assets/image/banner/banner1.png" alt="">
-                        </td>
-                        <td class="banner_title">2024大選推廣</td>
-                        <td class="banner_status">
-                            <SwitchBtn />
-                        </td>
-                        <td class="banner_operate">
-                            <button @click="showLightbox">
-                                <img src="../../public/images/icon/icon_revise.png" alt="">修改
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div>
+                <Search />
+            </div>
+            <div class="banner_table">
+                <table>
+                    <thead>
+                        <td>消息編號</td>
+                        <td>輪播圖預覽</td>
+                        <td>主旨</td>
+                        <td>啟用狀態</td>
+                        <td>操作</td>
+                    </thead>
+                    <tbody>
+                        <tr :key="index" v-for="index in 6">
+                            <td class="banner_id">1</td>
+                            <td class="banner_pic">
+                                <img src="../assets/image/banner/banner1.png" alt="">
+                            </td>
+                            <td class="banner_title">2024大選推廣</td>
+                            <td class="banner_status">
+                                <SwitchBtn />
+                            </td>
+                            <td class="banner_operate">
+                                <button @click="showLightbox">
+                                    <img src="../../public/images/icon/icon_revise.png" alt="">修改
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
     <Lightbox ref="lightbox" lightboxType="true">
@@ -38,16 +43,17 @@
                     <td>消息編號</td>
                     <td>輪播圖預覽(電腦)</td>
                     <td>輪播圖預覽(手機)</td>
+                    <td>啟用狀態</td>
                 </thead>
                 <tbody>
                     <tr>
                         <td>1</td>
-                        <td><img src="../assets/image/banner/banner1.png" alt=""></td>
-                        <td><div class="mobil_pic"><img src="../assets/image/banner/banner1_w350.png" alt=""></div></td>
+                        <td><div class="pc_pic"><img src="../assets/image/banner/banner1.png" alt=""></div></td>
+                        <td><div class="mobile_pic"><img src="../assets/image/banner/banner1_w350.png" alt=""></div></td>
+                        <td><SwitchBtn/></td>
                     </tr>
                 </tbody>
             </table>
-
         </div>
     
 
@@ -55,25 +61,82 @@
 </template>
 <script>
 import MainHeader from "../components/MainHeader.vue";
-import Dropdown from "../components/Dropdown.vue";
 import Lightbox from "../components/Lightbox.vue";
 import SwitchBtn from "../components/switch_btn.vue";
-export default {
-    data() {
-        return {
+import Search from "../components/SearchBtn.vue";
+import dropDown from "../components/Dropdown.vue";
+export default{
+    data(){
+        return{
         };
     },
-    components: {
-        MainHeader,
-        Dropdown,
-        Lightbox,
-        SwitchBtn
+    components:{
+    MainHeader,
+    Lightbox,
+    SwitchBtn,
+    Search,
+    dropDown
 
-    },
+},
     methods: {
-        showLightbox() {
+        showLightbox(){
             this.$refs.lightbox.showLightbox = true;
         }
     },
 }
 </script>
+<style lang="scss">
+@import "../assets/scss/style.scss";
+.banner_lightbox {
+    table {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        thead {
+            display: flex;
+            justify-content: space-around;
+            width: 800px;
+            height: 45px;
+            background: black;
+            text-align: center;
+            color: white;
+            td {
+                line-height: 48px;
+            }
+        }
+        tbody {
+            width: 800px;
+            tr {
+                display: flex;
+                justify-content: space-around;
+                margin: 20px 0;
+                td:nth-child(1), td:nth-child(4) {
+                    width: 15%;
+                    text-align: center;
+                    margin: auto;
+                    display: flex;
+                    justify-content: center;
+                }
+                td:nth-child(2), td:nth-child(3) {
+                    margin: auto;
+                }
+                .pc_pic {
+                    max-width: 220px;
+                    img {
+                        width: 100%;
+                    }
+                }
+                .mobile_pic {
+                    max-width: 200px;
+                    img {
+                        width:100%;
+                    }
+                }
+            }
+
+        }
+
+    }
+}
+
+</style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -3,38 +3,73 @@
     <Dropdown />
     <div class="news">
         <div class="news_container">
-            <table>
-                <thead>
-                    <td>編號</td>
-                    <td>主圖預覽</td>
-                    <td>新聞標題</td>
-                    <td>最後編輯日期</td>
-                    <td>啟用狀態</td>
-                    <td>操作</td>
-                </thead>
-                <tbody>
-                    <tr :key="index" v-for="index in 6">
-                        <td class="news_id">1</td>
-                        <td class="news_pic">
-                            <img src="../assets/image/news/news.png" alt="">
-                        </td>
-                        <td class="news_title">2024大選推廣</td>
-                        <td class="news_edit_date">2021/1/2</td>
-                        <td class="news_status">
-                            <SwitchBtn />
-                        </td>
-                        <td class="news_operate">
-                            <button @click="showLightbox">
-                                <img src="../../public/images/icon/icon_revise.png" alt="">修改
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div>
+                <Search />
+            </div>
+            <div class="news_table">
+                <table>
+                    <thead>
+                        <td>編號</td>
+                        <td>主圖預覽</td>
+                        <td>新聞標題</td>
+                        <td>最後編輯日期</td>
+                        <td>啟用狀態</td>
+                        <td>操作</td>
+                    </thead>
+                    <tbody>
+                        <tr :key="index" v-for="index in 6">
+                            <td class="news_id">1</td>
+                            <td class="news_pic">
+                                <img src="../assets/image/news/news.png" alt="">
+                            </td>
+                            <td class="news_title">2024大選推廣</td>
+                            <td class="news_edit_date">2021/1/2</td>
+                            <td class="news_status">
+                                <SwitchBtn />
+                            </td>
+                            <td class="news_operate">
+                                <button @click="showLightbox">
+                                    <img src="../../public/images/icon/icon_revise.png" alt="">修改
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-    <Lightbox ref="lightbox">
-
+    <Lightbox ref="lightbox" lightboxType="true">
+        <div class="news_lightbox">
+            <div class="news_title"><label>新聞標題:</label><input type="text"></div>
+            <div class="news_author"><label>作者:</label><input type="text"></div>
+            <div class="news_introduction"><label>引言:</label><textarea name="" id="" cols="30" rows="10"></textarea></div>
+            <div class="news_upload_pic">
+                <label for="">主要圖片上傳:</label>
+                <div class="news_main_pic"><img src="" alt=""></div>
+                <div class="news_file_name">
+                    <input type="file">
+                    <p>檔名:<span></span></p>
+                </div>
+            </div>
+            <div class="news_content">
+                <p>新聞內文</p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div class="news_marquee?">
+                <p>是否啟用跑馬燈</p>
+                <select name="" id="">
+                    <option value="是">是</option>
+                    <option value="否">否</option>
+                </select>      
+            </div>
+            <div class="news_marquee_status">
+                <p>啟用狀態</p>
+                <select name="" id="">
+                    <option value="是">是</option>
+                    <option value="否">否</option>
+                </select>      
+            </div>
+        </div>
     </Lightbox>
 </template>
 <script>
@@ -42,6 +77,8 @@ import MainHeader from "../components/MainHeader.vue";
 import Dropdown from "../components/Dropdown.vue";
 import Lightbox from "../components/Lightbox.vue";
 import SwitchBtn from "../components/switch_btn.vue";
+import Search from "../components/SearchBtn.vue";
+
 export default {
     data() {
         return {
@@ -51,8 +88,8 @@ export default {
         MainHeader,
         Dropdown,
         Lightbox,
-        SwitchBtn
-
+        SwitchBtn,
+        Search,
     },
     methods: {
         showLightbox() {
@@ -61,3 +98,20 @@ export default {
     },
 }
 </script>
+<style lang="scss">
+@import "../assets/scss/style.scss";
+.news_lightbox {
+    div {
+        display: flex; 
+        align-items: center;
+        label {
+            width: min-content;
+            padding: 10px;
+        }
+
+    }
+
+
+}
+
+</style>
