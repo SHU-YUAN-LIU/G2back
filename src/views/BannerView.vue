38af8215e7303d6103c2c85a9dabc86c1a4ed57c
Min-Syuan <annie0910610569gmail.com>
2024-02-09 00:24:48 +0800

    解衝突

--- src/views/BannerView.vue
+++ src/views/BannerView.vue
@@ -3,14 +3,6 @@
     <dropDown />
     <div class="banner">
         <div class="banner_container">
-<<<<<<< HEAD
-            <div>
-                <Search />
-            </div>
-            <div class="banner_table">
-                <table>
-                    <thead>
-=======
             <!-- 搜尋框 -->
             <div class="banner-btn">
                 <SearchBtn :placeholder="placeholder" />
@@ -21,27 +13,18 @@
                 <table class="table table-hover" style="position: relative;">
                     <!-- 表頭 -->
                     <thead align='center'>
->>>>>>> dev
                         <td>消息編號</td>
                         <td>輪播圖預覽</td>
                         <td>主旨</td>
                         <td>啟用狀態</td>
                         <td>操作</td>
                     </thead>
-<<<<<<< HEAD
-                    <tbody>
-                        <tr :key="index" v-for="index in 6">
-                            <td class="banner_id">1</td>
-                            <td class="banner_pic">
-                                <img src="../assets/image/banner/banner1.png" alt="">
-=======
                     <!-- 欄位內容 -->
                     <tbody>
                         <tr v-for="index in 6" valign="middle" align='center'>
                             <td class="banner_no">{{ index }}.</td>
                             <td class="banner_pic">
                                 <img src="../assets/image/index/banner1.png" alt="">
->>>>>>> dev
                             </td>
                             <td class="banner_title">2024大選推廣</td>
                             <td class="banner_status">
@@ -61,24 +44,6 @@
     <!-- 燈箱架構 -->
     <Lightbox ref="lightbox" lightboxType="true">
         <div class="banner_lightbox">
-<<<<<<< HEAD
-            <table>
-                <thead>
-                    <td>消息編號</td>
-                    <td>輪播圖預覽(電腦)</td>
-                    <td>輪播圖預覽(手機)</td>
-                    <td>啟用狀態</td>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td><div class="pc_pic"><img src="../assets/image/banner/banner1.png" alt=""></div></td>
-                        <td><div class="mobile_pic"><img src="../assets/image/banner/banner1_w350.png" alt=""></div></td>
-                        <td><SwitchBtn/></td>
-                    </tr>
-                </tbody>
-            </table>
-=======
             <!-- --------------------------------- -->
             <div class="banner-row-group">
                 <div class="banner_title">
@@ -103,7 +68,6 @@
                 </div>
                 <hr>
             </div>
->>>>>>> dev
         </div>
     </Lightbox>
 </template>
@@ -114,25 +78,6 @@ import axios from "axios";
 import MainHeader from "../components/MainHeader.vue";
 import Lightbox from "../components/Lightbox.vue";
 import SwitchBtn from "../components/switch_btn.vue";
-<<<<<<< HEAD
-import Search from "../components/SearchBtn.vue";
-import dropDown from "../components/Dropdown.vue";
-export default{
-    data(){
-        return{
-        };
-    },
-    components:{
-    MainHeader,
-    Lightbox,
-    SwitchBtn,
-    Search,
-    dropDown
-
-},
-    methods: {
-        showLightbox(){
-=======
 import SearchBtn from "../components/SearchBtn.vue";
 import Dropdown from "../components/Dropdown.vue";
 import addBtn from "../components/addBtn.vue";
@@ -157,7 +102,6 @@ export default {
     },
     methods: {
         showLightbox(product_no) {
->>>>>>> dev
             this.$refs.lightbox.showLightbox = true;
             // console.log(this.productdata);
             document.body.style.overflow = 'hidden';
@@ -167,64 +111,6 @@ export default {
 }
 
 </script>
-<<<<<<< HEAD
-<style lang="scss">
-@import "../assets/scss/style.scss";
-.banner_lightbox {
-    table {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        thead {
-            display: flex;
-            justify-content: space-around;
-            width: 800px;
-            height: 45px;
-            background: black;
-            text-align: center;
-            color: white;
-            td {
-                line-height: 48px;
-            }
-        }
-        tbody {
-            width: 800px;
-            tr {
-                display: flex;
-                justify-content: space-around;
-                margin: 20px 0;
-                td:nth-child(1), td:nth-child(4) {
-                    width: 15%;
-                    text-align: center;
-                    margin: auto;
-                    display: flex;
-                    justify-content: center;
-                }
-                td:nth-child(2), td:nth-child(3) {
-                    margin: auto;
-                }
-                .pc_pic {
-                    max-width: 220px;
-                    img {
-                        width: 100%;
-                    }
-                }
-                .mobile_pic {
-                    max-width: 200px;
-                    img {
-                        width:100%;
-                    }
-                }
-            }
-
-        }
-
-    }
-}
-
-</style>
-=======
 
 
 
->>>>>>> dev
