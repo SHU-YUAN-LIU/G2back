62a149ee379b71e2872fb69a8a307d1cdf5c3f82
ubudubud <ubud301@hotmail.com>
2024-02-21 10:09:26 +0800

    商品後台燈箱

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,88 +1,105 @@
 <template>
-    <MainHeader />
-    <Dropdown />
-    <div class="product">
-        <div class="product_container">
-            <div class="product-btn">
-                <SearchBtn :placeholder="placeholder" />
-                <addBtn />
-            </div>
-            <div class="product_table">
-                <table class="table table-hover" style="position: relative;">
-
-                    <thead>
-                        <td>商品編號</td>
-                        <td>商品圖片</td>
-                        <td>商品名稱</td>
-                        <td>類別</td>
-                        <td>售價</td>
-                        <td>是否上架</td>
-                        <td>操作</td>
-                    </thead>
-                    <tbody>
-                        <tr v-for="item in findproductsdata" valign="middle" align='center'>
-                            <td class="product_no">{{ item.product_no }}</td>
-                            <td>
-                                <img class="img-fluid" style="max-width: 80px;"
-                                    :src="getproductpic(item.product_pic1)">
-                            </td>
-                            <td class="product_name">{{ item.product_name }}</td>
-                            <td class="product_class">{{ item.product_class }}</td>
-                            <td class="product_price">{{ item.price }}</td>
-                            <td>
-                                <SwitchBtn />
-                            </td>
-                            <td class="product_operate">
-                                <button @click="showLightbox(item.product_no)">
-                                    <img src="/public/images/icon/icon_revise.png" alt="">修改
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+  <MainHeader />
+  <Dropdown />
+  <div class="product">
+    <div class="product_container">
+      <div class="product-btn">
+        <SearchBtn :placeholder="placeholder" />
+        <addBtn />
+      </div>
+      <div class="product_table">
+        <table class="table table-hover" style="position: relative">
+          <thead>
+            <td>商品編號</td>
+            <td>商品圖片</td>
+            <td>商品名稱</td>
+            <td>類別</td>
+            <td>售價</td>
+            <td>是否上架</td>
+            <td>操作</td>
+          </thead>
+          <tbody>
+            <tr v-for="item in findproductsdata" valign="middle" align="center">
+              <td class="product_no">{{ item.product_no }}</td>
+              <td>
+                <img
+                  class="img-fluid"
+                  style="max-width: 80px"
+                  :src="getproductpic(item.product_pic1)"
+                />
+              </td>
+              <td class="product_name">{{ item.product_name }}</td>
+              <td class="product_class">{{ item.product_class }}</td>
+              <td class="product_price">{{ item.price }}</td>
+              <td>
+                <SwitchBtn />
+              </td>
+              <td class="product_operate">
+                <button @click="showLightbox(item.product_no)">
+                  <img src="/public/images/icon/icon_revise.png" alt="" />修改
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <!-- 燈箱架構 -->
-    <Lightbox ref="lightbox" lightboxType="true">
-        <div class="product_lightbox">
-            <!-- --------------------------------- -->
-            <div class="product-row-group">
-                <p class="product-title-bar ">圖片/屬性</p>
-                <div class="product-row">
-                    <strong>商品名稱:</strong>
-                    <input class="form-control" type="text" placeholder="請輸入名稱">
-                </div>
-                <div class="product-row">
-                    <strong>商品類別:</strong>
-                    <input class="form-control" type="text" placeholder="請輸入類別">
-                </div>
-                <div class="product-row">
-                    <strong>商品售價:</strong>
-                    <input class="form-control" type="text" placeholder="請輸入售價">
-                </div>
-                <div class="product-row">
-                    <strong>商品數量:</strong>
-                    <input class="form-control" type="text" placeholder="請輸入數量">
-                </div>
-                <hr>
-                <div class="product-row">
-                    <strong>狀態:</strong>
-                    <select class="form-select">
-                        <option selected></option>
-                        <option value="A">上架</option>
-                        <option value="IA">下架</option>
-                    </select>
-                </div>
-                <hr>
-                <div class="product-row">
-                    <strong>上傳圖片:</strong>
-                    <input class="form-control" type="file" id="formFileMultiple" multiple>
-                </div>
-            </div>
-            <!-- ------------------------- -->
+  </div>
+  <!-- 燈箱架構 -->
+  <Lightbox ref="lightbox" lightboxType="true">
+    <div class="product_lightbox">
+      <!-- --------------------------------- -->
+      <div class="product-pic-group">
+        <div class="big-pic">上傳圖片</div>
+        
+        <div class="s-group">
+          <div class="small-pic">上傳</div>
+          <div class="small-pic">上傳</div>
+          <div class="small-pic">上傳</div>
+          <div class="small-pic">上傳</div>
         </div>
-    </Lightbox>
+      </div>
+      <div class="product-row-group">
+        <p class="product-title-bar">圖片/屬性</p>
+        <div class="product-row">
+          <strong>商品名稱:</strong>
+          <input class="form-control" type="text" placeholder="請輸入名稱" />
+        </div>
+        <div class="product-row">
+          <strong>商品類別:</strong>
+          <input class="form-control" type="text" placeholder="請輸入類別" />
+        </div>
+        <div class="product-row">
+          <strong>商品售價:</strong>
+          <input class="form-control" type="text" placeholder="請輸入售價" />
+        </div>
+        <div class="product-row">
+          <strong>商品數量:</strong>
+          <input class="form-control" type="text" placeholder="請輸入數量" />
+        </div>
+        <hr />
+        <div class="product-row">
+          <strong>狀態:</strong>
+          <select class="form-select">
+            <option selected></option>
+            <option value="A">上架</option>
+            <option value="IA">下架</option>
+          </select>
+        </div>
+        <hr />
+        <div class="product-row">
+          <strong>上傳圖片:</strong>
+          <input
+            class="form-control"
+            type="file"
+            id="formFileMultiple"
+            multiple
+          />
+        </div>
+      </div>
+      <!-- ------------------------- -->
+    </div>
+  </Lightbox>
 </template>
 
 <script>
@@ -94,50 +111,53 @@ import SearchBtn from "../components/SearchBtn.vue";
 import Dropdown from "../components/Dropdown.vue";
 import addBtn from "../components/addBtn.vue";
 export default {
-    data() {
-        return {
-            placeholder: '請輸入關鍵字',
-            productsdata: [],
-            findproductsdata: [],
-            lightboxdata: [],
-            lightbox_num: 0,
-        };
+  data() {
+    return {
+      placeholder: "請輸入關鍵字",
+      productsdata: [],
+      findproductsdata: [],
+      lightboxdata: [],
+      lightbox_num: 0,
+    };
+  },
+  components: {
+    MainHeader,
+    Lightbox,
+    SwitchBtn,
+    SearchBtn,
+    Dropdown,
+    addBtn,
+  },
+  created() {
+    this.getData();
+  },
+  mounted() {
+    document.title = "青年進補黨(後台) - 商品管理";
+  },
+  methods: {
+    getproductpic(src) {
+      return (
+        `${import.meta.env.VITE_RESOURCE_URL}images/product/product_data/` + src
+      );
     },
-    components: {
-        MainHeader,
-        Lightbox,
-        SwitchBtn,
-        SearchBtn,
-        Dropdown,
-        addBtn,
+    showLightbox(product_no) {
+      this.$refs.lightbox.showLightbox = true;
+      // console.log(this.productdata);
+      document.body.style.overflow = "hidden";
     },
-    created() {
-        this.getData();
+    getData() {
+      axios
+        .get(`${import.meta.env.VITE_PHP_URL}` + "/productDataGetAll.php")
+        .then((res) => {
+          this.productsdata = res.data.products;
+          this.findproductsdata = res.data.products;
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
     },
-    mounted() {
-        document.title = "青年進補黨(後台) - 商品管理";
-    },
-    methods: {
-        getproductpic(src) {
-            return `${import.meta.env.VITE_RESOURCE_URL}images/product/product_data/` + src;
-        },
-        showLightbox(product_no) {
-            this.$refs.lightbox.showLightbox = true;
-            // console.log(this.productdata);
-            document.body.style.overflow = 'hidden';
-        },
-        getData() {
-            axios.get(`${import.meta.env.VITE_PHP_URL}` + "/productDataGetAll.php")
-                .then(res => {
-                    this.productsdata = res.data.products;
-                    this.findproductsdata = res.data.products;
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
-        },
-    }
-}
+  },
+};
 </script>
 
 <style></style>
